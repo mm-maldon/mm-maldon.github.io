72404037e7c2f182a961cdfa065a6ee175e56257
Malachi Maldonado <m.maldonado1792@gmail.com>
2021-04-18 17:46:47 -0700

    initial commit

--- spritescript.js
+++ spritescript.js
@@ -1,13 +1,11 @@
- //This script handles the sprite animation of the torch sprites on the top of each page.
+ //This script handles the sprite animation of the torches on the top of each page.
  (function(){ "use strict";
     var display_left = {
-
         canv: document.getElementById("l-canvas"),
         cont: document.getElementById("l-canvas").getContext("2d"),
     }
 
     var display_right = {
-
         canv: document.getElementById("r-canvas"),
         cont: document.getElementById("r-canvas").getContext("2d"),
     }
@@ -21,32 +19,31 @@
         this.frame_set = frame_set;
     }
 
-        //Animation functions
-        Animation.prototype = {
+    //Animation functions
+    Animation.prototype = {
 
-            change: function(frame_set, delay){
-                if(this.frame_set != frame_set){
-                    this.count = 0;
-                    this.delay = delay;
-                    this.frame_index = 0;
-                    this.frame_set = frame_set;
-                    this.frame = this.frame_set[this.frame_index];
-                }
-            },
+        change: function(frame_set, delay){
+            if(this.frame_set != frame_set){
+                this.count = 0;
+                this.delay = delay;
+                this.frame_index = 0;
+                this.frame_set = frame_set;
+                this.frame = this.frame_set[this.frame_index];
+            }
+        },
 
-            //The function that does the work. Will loop through a frame set and change the
-            //current image tied to the object referenced
-            update: function(){
-                this.count ++;
+        //The function that does the work. Will loop through a frame set and change the
+        //current image tied to the object referenced
+        update: function(){
+            this.count ++;
 
-                if (this.count >= this.delay) {
-                    this.count = 0;
-                    this.frame_index = (this.frame_index == this.frame_set.length - 1) ? 0 : this.frame_index + 1;
-                    this.frame = this.frame_set[this.frame_index];
-                }
+            if (this.count >= this.delay) {
+                this.count = 0;
+                this.frame_index = (this.frame_index == this.frame_set.length - 1) ? 0 : this.frame_index + 1;
+                this.frame = this.frame_set[this.frame_index];
             }
         }
-
+    }
 
     var torch = {
 
@@ -87,6 +84,5 @@
 
     torch.anim.change(t_sheet.frame_list, 6);
     request = window.requestAnimationFrame(loop);
-
 
  })();
